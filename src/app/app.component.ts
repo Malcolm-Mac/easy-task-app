7776e0ec38526fafeb0beb5757a04aceaf18d0d0
Malcolm-Mac <malcolmprojects12@gmail.com>
2024-07-21 21:33:15 +0200

    learnt @input and @output directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from '../mock/dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'easy-task-app';
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+  name?: string;
+
+  get selectedUser(){
+    return this.users.find((user)=>user.id===this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  computed,
+  input,
+  output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
